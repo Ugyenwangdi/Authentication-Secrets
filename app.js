0fd09af3664a1735c252665b1ef775142402e926
Ugyenwangdi <12190105.gcit@rub.edu.bt>
2023-01-30 01:29:29 +0600

    use bcrypt hash

--- app.js
+++ app.js
@@ -3,59 +3,69 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+// const md5 = require("md5");
+
+const saltRounds = 10;
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.set('strictQuery', true);
 main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/userDB');
-}
+};
 
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
 
-const secret = process.env.SOME_LONG_UNGUESSABLE_STRING;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+// const encrypt = require("mongoose-encryption");
+// const secret = process.env.SOME_LONG_UNGUESSABLE_STRING;
+// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
-    res.render("home")
+    res.render("home");
 });
 
 app.get("/login", function(req, res){
-    res.render("login")
+    res.render("login");
 });
 
 app.get("/register", function(req, res){
-    res.render("register")
+    res.render("register");
 });
 
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    })
 
-    newUser.save(function(err){
-        if (err){
-            console.log(err);
-        } else {
-            res.render("secrets");
-        }
-    })
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUser.save(function(err){
+            if (err){
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
+    });
+
+    
 });
 
 app.post("/login", function(req, res){
+
     const username = req.body.username;
     const password = req.body.password;
 
@@ -64,9 +74,11 @@ app.post("/login", function(req, res){
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if(result === true){
+                        res.render("secrets");
+                    }
+                });
             }
         }
     });
